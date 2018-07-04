cddf1703e44424b0455eba5378fb1b64778f3b4f
warcraftpao <warcraftpao@gmail.com>
2018-07-04 17:25:34 +0800

    add test validParentheses

---
+++ Leetcode/Easy/ValidParentheses.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Easy
+{
+    public class ValidParentheses
+    {
+        public static bool Valid(string input)
+        {
+  
+            var dic = new Dictionary<char ,char>();
+            //后出现的部分做key
+            dic.Add(']', '[');
+            dic.Add(')', '(');
+            dic.Add('}', '{');
+            var queue = new Stack();
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                if (dic.ContainsValue(input[i]))
+                    queue.Push(input[i]);
+                else if (dic.ContainsKey(input[i]))
+                {
+                    var c = (char) queue.Peek();
+                    if (c != dic[input[i]])
+                        return false;
+                    
+                    queue.Pop();
+                }
+            }
+
+            if (queue.Count > 0)
+                return false;
+
+            return true;
+        }
+    }
+}
--- TestLeetcode/UnitTest5.cs
+++ TestLeetcode/UnitTest5.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Collections.Generic;
 using Leetcode.DataStructure;
+using Leetcode.Easy;
 using Leetcode.Middle;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -17,6 +18,26 @@ namespace TestLeetcode
             var head = LinkedList.BuildListNode(new[] {1, 2, 3, 4, 5});
             head = RemoveNthFromEnd.S1(head, 2);
             Assert.AreEqual(head.Length(), 4);
+        }
+
+        [TestMethod]
+        public void TestValidParentheses()
+        {
+            var r = ValidParentheses.Valid("()");
+            Assert.AreEqual(r, true);
+
+            r = ValidParentheses.Valid("()[]{}");
+            Assert.AreEqual(r, true);
+
+            r = ValidParentheses.Valid("(]");
+            Assert.AreEqual(r, false);
+
+            r = ValidParentheses.Valid("([)]");
+            Assert.AreEqual(r, false);
+
+            r = ValidParentheses.Valid("{[]}");
+            Assert.AreEqual(r, true);
+ 
         }
     }
 }
