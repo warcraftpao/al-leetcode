91a1baba07a909577cf34ef762a19533c690c8e7
warcraftpao <warcraftpao@gmail.com>
2018-07-20 14:32:59 +0800

    add rotate iamge

---
+++ Leetcode/Middle/RotateImage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Middle
+{
+    public class RotateImage
+    {
+        //自己写的
+        //必须是n*n 的数组
+        //像剥皮一样一层层往里剥，所以大循环只要lenght/2,层数奇数最里面那层（1个值）不用动
+        //每层大循环四个数字一组循环复制。
+        public static void Rotate(int[,] arr)
+        {
+            var len = arr.GetLength(1);
+            var reduce = 1;
+            for (var i = 0; i < len / 2; i++)
+            {
+                var tmp = 0;
+                for (var j = i; j < len - reduce; j++ , reduce++)
+                {
+                    tmp = arr[i, j];
+                    arr[i, j] = arr[len -j -1, i];
+                    arr[len - j - 1, i] = arr[len - i -1, len - j -1];
+                    arr[len - i -1, len - j -1] = arr[j, len -i -1];
+                    arr[j, len - i - 1]  = tmp;
+                }
+            }
+        }
+    }
+}
--- TestLeetcode/UnitTest8.cs
+++ TestLeetcode/UnitTest8.cs
@@ -27,7 +27,36 @@ namespace TestLeetcode
             Assert.AreEqual(r, "1113213211");
         }
 
-        
+        [TestMethod]
+        public void TestRotateImage()
+        {
+            var arr = new int[3, 3]
+            {
+                {1, 2, 3,},
+                {4, 5, 6,},
+                {7, 8, 9,},
+
+
+            };
+            RotateImage.Rotate( arr);
+
+            Assert.AreEqual(arr[0,0], 7);
+
+            arr = new int[4, 4]
+            {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+
+                {13, 14, 15, 16},
+            };
+            RotateImage.Rotate(arr);
+
+            Assert.AreEqual(arr[2, 2], 7);
+            Assert.AreEqual(arr[2, 3], 3);
+            Assert.AreEqual(arr[3, 2], 8);
+            Assert.AreEqual(arr[3, 3], 4);
+        }
 
     }
 
