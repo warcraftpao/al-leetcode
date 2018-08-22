fcbc339d435c49971c85b29c59ec6defc4cb5a75
warcraftpao <warcraftpao@gmail.com>
2018-08-22 16:20:43 +0800

    add binary tree path sum 1

---
+++ Leetcode/BinaryTree/PathSum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Leetcode.DataStructure;
+
+namespace Leetcode.BinaryTree
+{
+    public class PathSum
+    {
+        public static bool HasPathSum(TreeNode root, int sum)
+        {
+            if (root == null) return false;
+            var left = sum - root.Val;
+            if (root.Left == null && root.Right == null && left == 0)
+                return true;
+            return HasPathSum(root.Left, left) || HasPathSum(root.Right, left);
+        }
+
+         
+    }
+}
---
+++ TestLeetcode/BinaryTree/TestTreePath.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Leetcode.BinaryTree;
+using Leetcode.DataStructure;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestLeetcode.BinaryTree
+{
+    [TestClass]
+    public class TestTreePath
+    {
+        [TestMethod]
+        public void TestPath1()
+        {
+            var root = new TreeNode(5);
+            root.Left = new TreeNode(4);
+            root.Right = new TreeNode(8);
+
+            root.Left.Left = new TreeNode(11);
+            root.Left.Left.Left = new TreeNode(7);
+            root.Left.Left.Right = new TreeNode(2);
+
+            root.Right.Left = new TreeNode(13);
+            root.Right.Right = new TreeNode(4);
+            root.Right.Right.Right = new TreeNode(1);
+
+            var r = PathSum.HasPathSum(root, 22);
+            Assert.AreEqual(r,true);
+
+            r = PathSum.HasPathSum(root, 13);
+            Assert.AreEqual(r, false);
+
+        }
+    }
+}
