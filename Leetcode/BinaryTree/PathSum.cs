9e998042e9f6f5e270a5f0c4558f3db6044ad107
warcraftpao <warcraftpao@gmail.com>
2018-08-23 13:21:28 +0800

    add binary tree path sum 1 ,2

--- Leetcode/BinaryTree/PathSum.cs
+++ Leetcode/BinaryTree/PathSum.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +19,49 @@ namespace Leetcode.BinaryTree
             return HasPathSum(root.Left, left) || HasPathSum(root.Right, left);
         }
 
-         
+        public static List<List<int>> GetPathSum(TreeNode root, int sum)
+        {
+            var result = new List<List<int>>();
+            var curr = new List<int>();
+            curr.Add(root.Val);
+            GetPathSum(result, root, sum -root.Val, curr);
+            return result;
+        }
+
+        private static void GetPathSum(List<List<int>> result, TreeNode root, int sum, List<int> curr)
+        {
+            //null不判断
+            if (root == null)
+                return;
+
+             
+            //符合条件的加入结果集
+            if (root.Left == null && root.Right == null && sum == 0)
+            {
+
+                var tmp = new List<int>();
+                foreach (var item in curr)
+                {
+                    tmp.Add(item);
+                }
+                result.Add(tmp);
+
+                return;
+            }
+
+            if(root.Left  != null)
+            {
+                curr.Add(root.Left.Val);
+                GetPathSum(result, root.Left, sum- root.Left.Val, curr);
+                curr.RemoveAt(curr.Count - 1);
+            }
+
+            if (root.Right != null)
+            {
+                curr.Add(root.Right.Val);
+                GetPathSum(result, root.Right, sum - root.Right.Val, curr);
+                curr.RemoveAt(curr.Count - 1);
+            }
+        }
     }
 }
--- TestLeetcode/BinaryTree/TestTreePath.cs
+++ TestLeetcode/BinaryTree/TestTreePath.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Leetcode.BinaryTree;
 using Leetcode.DataStructure;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -29,6 +30,28 @@ namespace TestLeetcode.BinaryTree
             r = PathSum.HasPathSum(root, 13);
             Assert.AreEqual(r, false);
 
+        }
+
+
+        [TestMethod]
+        public void TestGetPathSum()
+        {
+            var root = new TreeNode(5);
+            root.Left = new TreeNode(4);
+            root.Right = new TreeNode(8);
+
+            root.Left.Left = new TreeNode(11);
+            root.Left.Left.Left = new TreeNode(7);
+            root.Left.Left.Right = new TreeNode(2);
+
+            root.Right.Left = new TreeNode(13);
+            root.Right.Right = new TreeNode(4);
+            root.Right.Right.Right = new TreeNode(1);
+            root.Right.Right.Left = new TreeNode(5);
+
+            var r = PathSum.GetPathSum(root, 22);
+            Assert.AreEqual(r.Count, 2);
+            Assert.AreEqual(r.All( i=> i.Sum() == 22), true);
         }
     }
 }
