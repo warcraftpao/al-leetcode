f106e12b224e3dc4a3c6c132eba8512a309cf3b8
warcraftpao <warcraftpao@gmail.com>
2018-09-06 18:02:15 +0800

    add longest consective sequence

---
+++ Leetcode/Hard/LongestConsecutiveSequence.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Hard
+{
+    //讲道理，这题能算hard。。。。
+    public   class LongestConsecutiveSequence
+    {
+        public static int GetLength(int[] arr)
+        {
+            Array.Sort(arr);
+            var longest = 1;
+            var curr = 1;
+            for (var i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] == arr[i - 1])
+                {
+                    curr++;
+                }
+                else
+                {
+                    longest = Math.Max(longest, curr);
+                    curr = 1;
+                }
+            }
+            return longest;
+        }
+    }
+}
