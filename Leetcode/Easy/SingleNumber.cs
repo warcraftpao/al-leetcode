61ae2d9ad3e5f89cffdb7ba49dfe2230c2937db8
warcraftpao <warcraftpao@gmail.com>
2018-10-22 17:25:31 +0800

    add single number 1

---
+++ Leetcode/Easy/SingleNumber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Easy
+{
+    public class SingleNumber1
+    {
+        public static int GetNumber_hashtable(int[] nums)
+        {
+            var dic =new HashSet<int>();
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (dic.Contains(nums[i]))
+                    dic.Remove(nums[i]);
+                else
+                    dic.Add(nums[i]);
+            }
+             
+            return dic.First();
+        }
+
+        //异或操作 ，相同返回0，不同返回1
+        //0和任何数异或返回任何数
+        //1和任何数异或等于任何数取反
+        //任何数和自己异或=0
+        public static int GetNumber_bit(int[] nums)
+        {
+            var a = 0;
+            //有相同的数字最终都变成0，0和唯一的数字异或=要求的数字
+            foreach (var num in nums)
+            {
+                a = a ^ num;
+            }
+            return a;
+        }
+    }
+}
---
+++ TestLeetcode/UnitTest10.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Leetcode.Easy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestLeetcode
+{
+    [TestClass]
+    public class UnitTest10
+    {
+        [TestMethod]
+        public void TestSingleNumber1_hashtable()
+        {
+            var r = SingleNumber1.GetNumber_hashtable(new[] {2, 2, 3, 3, 4, 4, 1, 5, 5, 6, 7, 8, 8, 7, 6});
+            Assert.AreEqual(r, 1);
+        }
+
+        [TestMethod]
+        public void TestSingleNumber1_bit()
+        {
+            var r = SingleNumber1.GetNumber_bit(new[] { 2, 2, 3, 3, 4, 4, 1, 5, 5, 6, 7, 8, 8, 7, 6 });
+            Assert.AreEqual(r, 1);
+        }
+    }
+}
