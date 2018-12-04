b901a834d2ca0ccf1f0c95aa02cc1688acd30a01
warcraftpao <warcraftpao@gmail.com>
2018-12-04 16:14:04 +0800

    link list cycle

---
+++ Leetcode/Easy/LinkListCycle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+using Leetcode.DataStructure;
+
+namespace Leetcode.Easy
+{
+    public  class LinkListCycle
+    {
+        public static bool IsCycle(MyLinkList head)
+        {
+            var rabbit = head.Next;
+            var turtle = head.Next;
+
+            //如果没有回路，rabbit总是先到终点，所以rabbit变成null或者next是null，没有回路
+            while (rabbit != null && rabbit.Next != null)
+            {
+                turtle = turtle.Next;
+                //如果有回路（不管是大回环，还是6字形，rabbit和turtle早晚都会跑进回路，那么rabbit总会追上trutle
+                rabbit = rabbit.Next.Next;
+                if (rabbit == turtle)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
--- TestLeetcode/UnitTest10.cs
+++ TestLeetcode/UnitTest10.cs
@@ -88,5 +88,81 @@ namespace TestLeetcode
             Assert.AreEqual(result.Contains("cats and dog"),true);
             Assert.AreEqual(result.Contains("cat sand dog"), true);
         }
+
+        [TestMethod]
+        public void TestLinkListCycle_notcycle()
+        {
+            var head = MyLinkList.BuildListNodeFromArray(new[] { 1, 2, 3, 4, 5, 6, 7 });
+            var result = LinkListCycle.IsCycle(head);
+            Assert.AreEqual(result,false);
+          
+        }
+
+        
+        [TestMethod]
+        //大回环的环形
+        public void TestLinkListCycle_iscycle_round()
+        {
+            var head = BuildCycle_round();
+            var result = LinkListCycle.IsCycle(head);
+            Assert.AreEqual(result, true);
+
+        }
+
+        [TestMethod]
+        //6字形的环形
+        public void TestLinkListCycle_iscycle_six()
+        {
+            var head = BuildCycle_six();
+            var result = LinkListCycle.IsCycle(head);
+            Assert.AreEqual(result, true);
+
+        }
+
+
+        private MyLinkList BuildCycle_round()
+        {
+            var head = new MyLinkList();
+            var node1 = new MyLinkList { Val = 1 };
+            var node2 = new MyLinkList { Val = 2 };
+            var node3 = new MyLinkList { Val = 3 };
+            var node4 = new MyLinkList { Val = 4 };
+            var node5 = new MyLinkList { Val = 5 };
+            var node6 = new MyLinkList { Val = 6 };
+            var node7 = new MyLinkList { Val = 7 };
+            head.Next = node1;
+            node1.Next = node2;
+            node2.Next = node3;
+            node3.Next = node4;
+            node4.Next = node5;
+            node5.Next = node6;
+            node6.Next = node7;
+            node7.Next = node1;
+
+            return head;
+        }
+
+
+        private MyLinkList BuildCycle_six()
+        {
+            var head = new MyLinkList();
+            var node1 = new MyLinkList { Val = 1 };
+            var node2 = new MyLinkList { Val = 2 };
+            var node3 = new MyLinkList { Val = 3 };
+            var node4 = new MyLinkList { Val = 4 };
+            var node5 = new MyLinkList { Val = 5 };
+            var node6 = new MyLinkList { Val = 6 };
+            var node7 = new MyLinkList { Val = 7 };
+            head.Next = node1;
+            node1.Next = node2;
+            node2.Next = node3;
+            node3.Next = node4;
+            node4.Next = node5;
+            node5.Next = node6;
+            node6.Next = node7;
+            node7.Next = node3;
+
+            return head;
+        }
     }
 }
