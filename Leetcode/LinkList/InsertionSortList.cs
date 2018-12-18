d82524e2ac8099cbdd37e90aa858ea9a6b9e3cdf
warcraftpao <warcraftpao@gmail.com>
2018-12-18 14:27:37 +0800

    insertion sort linklist

---
+++ Leetcode/LinkList/InsertionSortList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Leetcode.DataStructure;
+
+namespace Leetcode.LinkList
+{
+    public  class InsertionSortList
+    {
+        public static MyLinkList Sort(MyLinkList head)
+        {
+            //一个新链表的头
+            var dummy = new MyLinkList {Val = -1};
+            //当前链表循环的头
+            var  curr =head.Next;
+
+            while (curr != null)
+            {
+                //每次都从新链表的头开始循环，每次开始的时候新链表都是排序的（第一次只有一个元素的时候不用排序）
+                //新链表循环的标志
+                var pre = dummy;
+                //新链表都是排序过的，而且每个值都需要比较，停留的位置，就是curr需要在之后插入的位置
+                while (pre.Next != null && pre.Next.Val < curr.Val)
+                {
+                    pre = pre.Next;
+                }
+                 
+                var tmp = curr.Next;
+                // 把curr插到pre之后，所以其实这时候有两个链表了，老链表curr在后移，新链表在增加，而且不用考虑是不是插入在最后或者中间
+                curr.Next = pre.Next;
+                pre.Next = curr;
+                 
+                curr = tmp;
+            }
+
+            return dummy;
+        }
+    }
+}
---
+++ TestLeetcode/FamilyTest/TestLinkList3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Leetcode.DataStructure;
+using Leetcode.LinkList;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestLeetcode.FamilyTest
+{
+    [TestClass]
+    public class TestLinkList3
+    {
+        [TestMethod]
+        public void TestInsertionSortList()
+        {
+            var head = MyLinkList.BuildListNodeFromArray(new[] {3, 5, 2, 6, 7, 4, 1 });
+            var dummy = InsertionSortList.Sort(head);
+            Assert.AreEqual(dummy.Next.Val,1);
+            Assert.AreEqual(dummy.Next.Next.Next.Val, 3);
+            Assert.AreEqual(dummy.Next.Next.Next.Next.Next.Val, 5);
+            Assert.AreEqual(dummy.Next.Next.Next.Next.Next.Next.Next.Val, 7);
+             
+        }
+    }
+}
