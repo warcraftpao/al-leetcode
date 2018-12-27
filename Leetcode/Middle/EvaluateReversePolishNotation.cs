0953cfff84fb57581b87f440b13abcea6b70b2ca
warcraftpao <warcraftpao@gmail.com>
2018-12-27 17:31:54 +0800

    evaluate reserve polish notation

---
+++ Leetcode/Middle/EvaluateReversePolishNotation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Middle
+{
+    public class EvaluateReversePolishNotation
+    {
+        private static List<string> Operator = new List<string> {"+","-","*","/"};
+        public static int EvalRPN(string[] tokens)
+        {
+            var stack = new Stack<string>();
+             
+            int lastCalcResult = 1 ; //  这只是为了初始化，什么值没关系
+            for (var i = 0; i < tokens.Length; i++)
+            {
+                //是操作符
+                if (Operator.Contains(tokens[i]))
+                {
+                    var b = Convert.ToInt32(stack.Pop());
+                    var a = Convert.ToInt32(stack.Pop());
+                    lastCalcResult = Calc(a, b, tokens[i]);
+                    //把结果也入栈，简化操作，因为一开始总归有2个数字，之后可以只有一个数字需要用到上次结算的结果
+                    stack.Push(lastCalcResult.ToString());
+                }
+                else //不是操作符
+                {
+                    stack.Push(tokens[i]);
+                }
+            }
+
+            return lastCalcResult;
+        }
+
+        private static int Calc(int a, int b, string op)
+        {
+            switch (op)
+            {
+                case "+":
+                    return a + b;
+                case "-":
+                    return a - b;
+                case "*":
+                    return a*b;
+                default:
+                    return a/b;
+            }
+        }
+    }
+}
---
+++ TestLeetcode/UnitTest11.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Leetcode.Middle;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestLeetcode
+{
+    [TestClass]
+    public class UnitTest11
+    {
+        [TestMethod]
+        public void TestEvaluateReversePolishNotation()
+        {
+            var tokens = new[] {"2", "1", "+", "3", "*"};
+            var r = EvaluateReversePolishNotation.EvalRPN(tokens);
+            Assert.AreEqual(r, 9);
+
+            tokens = new[] { "4", "13", "5", "/", "+" };
+            r = EvaluateReversePolishNotation.EvalRPN(tokens);
+            Assert.AreEqual(r, 6);
+
+            tokens = new[] { "10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+" };
+            r = EvaluateReversePolishNotation.EvalRPN(tokens);
+            Assert.AreEqual(r, 22d);
+        }
+    }
+}
