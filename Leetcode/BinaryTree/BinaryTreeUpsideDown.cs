94a408443a4f876ccd423b8ee80b1c4dbb5d0698
warcraftpao <warcraftpao@gmail.com>
2019-01-17 17:30:02 +0800

    upside down binary tree

---
+++ Leetcode/BinaryTree/BinaryTreeUpsideDown.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.BinaryTree
+{
+    public class BinaryTreeUpsideDown
+    {
+        //http://206.81.6.248:12306/leetcode/binary-tree-upside-down/description
+        //Given a binary tree where all the right nodes are either leaf nodes with a sibling (a left node that shares the same parent node) or empty, 
+        //flip it upside down and turn it into a tree where the original right nodes turned into left leaf nodes. Return the new root.
+
+        //意思就是一个二叉树，右节点要么是空，要么必然左右都有（写的好别扭），要求上下翻转，右节点变成新的左节点（父节点变右节点）
+
+        //https://www.cnblogs.com/grandyang/p/5172838.html
+
+        //暂时懒得做
+    }
+}
