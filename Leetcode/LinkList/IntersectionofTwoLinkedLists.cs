787fa943b4eed93a7349f7c259544a5ff7e7749f
warcraftpao <warcraftpao@gmail.com>
2019-01-21 18:10:18 +0800

    intersection of 2 linked list

---
+++ Leetcode/LinkList/IntersectionofTwoLinkedLists.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Leetcode.DataStructure;
+
+namespace Leetcode.LinkList
+{
+    //这类题关键是要理解英文原意
+    //就是说俩链表要么有交集，在交集发生后后面的元素都相同的，要么就干脆没有交集
+    //abc 和 xyz 或者 abcdefg和xyzefg的意思，e就是最早的公共元素，之后的相同
+    public class IntersectionofTwoLinkedLists
+    {
+        //假设他们有交集，他们交集的长度必相同的，所以len1和len2的差值是len的话，较长链表的长度-len后，长度肯定还是大于等于公共部分
+        //所以让长的那个链表先多走len步后，每一步判定2个链表的值是否相同就行了
+        public static MyLinkList GetIntersection1(MyLinkList node1, MyLinkList node2)
+        {
+            var len1 = node1.Length();
+            var len2 = node2.Length();
+            
+            if(len1>  len2)
+            {
+                while (len1 > len2)
+                {
+                    node1 = node1.Next;
+                    len1--;
+                }
+            }
+            else if( len1 < len2)
+            {
+                while (len1 < len2)
+                {
+                    node2 = node2.Next;
+                    len2--;
+                }
+            }
+
+            while (node1 != null)
+            {
+                if (node1 == node2)
+                    return node1;
+
+                node1 = node1.Next;
+                node2 = node2.Next;
+            }
+
+            return null;
+        }
+    }
+}
--- TestLeetcode/FamilyTest/TestLinkList3.cs
+++ TestLeetcode/FamilyTest/TestLinkList3.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 using Leetcode.DataStructure;
 using Leetcode.LinkList;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -18,6 +21,75 @@ namespace TestLeetcode.FamilyTest
             Assert.AreEqual(dummy.Next.Next.Next.Next.Next.Val, 5);
             Assert.AreEqual(dummy.Next.Next.Next.Next.Next.Next.Next.Val, 7);
              
+        }
+
+        [TestMethod]
+        public void TestIntersectionofTwoLinkedLists()
+        {
+            var list = BuildNode1();
+            var r = IntersectionofTwoLinkedLists.GetIntersection1(list[0], list[1]);
+            Assert.AreEqual(r.Val,5 );
+
+            list = BuildNode2();
+            r = IntersectionofTwoLinkedLists.GetIntersection1(list[0], list[1]);
+            Assert.AreEqual(r, null);
+        }
+
+        private List<MyLinkList> BuildNode2()
+        {
+            var node1= new MyLinkList {Val = 1};
+            var node2 = new MyLinkList { Val = 2 };
+            var node3 = new MyLinkList { Val = 3 }; ;
+            var node4 = new MyLinkList { Val = 4 };
+            var node5 = new MyLinkList { Val = 5 };
+            var node6 = new MyLinkList { Val = 6 };
+            var node7 = new MyLinkList { Val = 7};
+            var head1 = new MyLinkList { Val = -1 };
+            var head2 = new MyLinkList { Val = -2 };
+
+
+            //123567-----4567
+            head1.Next = node1;
+            node1.Next = node2;
+            node2.Next = node3;
+            node3.Next = node4;
+
+
+            head2.Next = node5;
+             node5.Next = node6;
+            node6.Next = node7;
+
+            return new List<MyLinkList> {head1,head2};
+        }
+
+        private List<MyLinkList> BuildNode1()
+        {
+            var node1= new MyLinkList {Val = 1};
+            var node2 = new MyLinkList { Val = 2 };
+            var node3 = new MyLinkList { Val = 3 }; ;
+            var node4 = new MyLinkList { Val = 4 };
+            var node5 = new MyLinkList { Val = 5 };
+            var node6 = new MyLinkList { Val = 6 };
+            var node7 = new MyLinkList { Val = 7};
+            var head1 = new MyLinkList { Val = -1 };
+            var head2 = new MyLinkList { Val = -2 };
+
+
+            //123567-----4567
+            head1.Next = node1;
+            node1.Next = node2;
+            node2.Next = node3;
+            node3.Next = node5;
+
+
+            head2.Next = node4;
+            
+
+            node4.Next = node5;
+            node5.Next = node6;
+            node6.Next = node7;
+
+            return new List<MyLinkList> {head1,head2};
         }
     }
 }
