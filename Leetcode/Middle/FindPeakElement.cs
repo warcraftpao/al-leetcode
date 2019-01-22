7e71641ea7077f2f414a3a185f5536a246d1033d
warcraftpao <warcraftpao@gmail.com>
2019-01-22 16:48:16 +0800

    find peak element

---
+++ Leetcode/Middle/FindPeakElement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Middle
+{
+    //注意 对数时间复杂度
+    public class FindPeakElement
+    {
+        //还是得binary search，因为只需要找到任意一个点就行了
+        //找到中间点，如果mid比 mid-1大，说明mid之后（含mid）肯定有peak，mid比mid-1小，说明min之前有个peak
+        public static int Find(int[] arr)
+        {
+            var left = 0;
+            var right = arr.Length - 1;
+            //满足条件至少还有两个元素吧，这时候left=mid，要么right减小了要么left+1了，退出循环
+            while (left < right)
+            {
+                var mid = (right + left)/2;
+                if (arr[mid] > arr[mid + 1]) //下降
+                    right = mid;
+                else //上升
+                    left = mid + 1;
+            }
+
+            return left;
+        }
+    }
+}
--- TestLeetcode/UnitTest12.cs
+++ TestLeetcode/UnitTest12.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Leetcode.Middle;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -27,5 +28,24 @@ namespace TestLeetcode
             Assert.AreEqual(r, false);
 
         }
+
+
+        [TestMethod]
+        public void TestFindPeakElement()
+        {
+            var r = FindPeakElement.Find(new[] {1, 2, 3, 2, 1, 2, 4, 5, 3, 1, 10});
+            Assert.AreEqual(new int[] {2,7,10}.Contains(r), true);
+
+             r = FindPeakElement.Find(new[] { 1, 2, 3});
+            Assert.AreEqual(r, 2);
+
+            r = FindPeakElement.Find(new[] { 1,2 });
+            Assert.AreEqual(r, 1);
+
+
+            r = FindPeakElement.Find(new[] { 1, 2,3,10,5,4 });
+            Assert.AreEqual(r, 3);
+        }
+
     }
 }
