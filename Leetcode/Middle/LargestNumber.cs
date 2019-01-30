8845f9a6cc591c03ebf4040fd171dacb72aba580
warcraftpao <warcraftpao@gmail.com>
2019-01-30 15:16:03 +0800

    largest number

---
+++ Leetcode/Middle/LargestNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Middle
+{
+    public  class LargestNumber 
+    {
+        
+        public static string GetLargest(int[] arr)
+        {
+            var list = arr.OrderByDescending(l => l.ToString(), new NumberCombineCompare());
+            return string.Join("", list);
+        } 
+    }
+
+    public class NumberCombineCompare : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            return string.CompareOrdinal($"{x}{y}", $"{y}{x}");
+        }
+    }
+}
---
+++ TestLeetcode/UnitTest13.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+using Leetcode.Middle;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestLeetcode
+{
+    [TestClass]
+    public class UnitTest13
+    {
+        [TestMethod]
+        public void TestLargestNumber()
+        {
+            var r = LargestNumber.GetLargest(new[] {3, 30, 34, 5, 9});
+            Assert.AreEqual(r, "9534330");
+        }
+    }
+}
