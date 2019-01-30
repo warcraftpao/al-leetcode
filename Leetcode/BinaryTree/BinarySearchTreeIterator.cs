69b7e63132b2cc5793a68cc4d29388f268d32ce0
warcraftpao <warcraftpao@gmail.com>
2019-01-30 11:29:11 +0800

    binary search tree inerator

--- Leetcode/BinaryTree/BinarySearchTreeIterator.cs
+++ Leetcode/BinaryTree/BinarySearchTreeIterator.cs
@@ -3,19 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization.Configuration;
+using Leetcode.DataStructure;
 
 namespace Leetcode.BinaryTree
 {
+    //way1 bst先序遍历 左中右全部入栈，next就不停出栈
+
+    //way2 节省空间的做法，因为bst必然是左小于右，所以可以不停入栈left，此时入栈顶必然是最小的
+    //next方法先出栈栈顶元素，然后再判断栈顶元素是否有right，如果有right必然是现在栈顶元素的右孩子，right入栈，再不停入栈right的left孩子
+
+
     public class BinarySearchTreeIterator
     {
-        //public bool HasNext()
-        //{
-            
-        //}
+        private Stack<TreeNode>  _stack = new Stack<TreeNode>();
+        public BinarySearchTreeIterator(TreeNode root)
+        {
+            while (root != null)
+            {
+                _stack.Push(root);
+                root = root.Left;
+            }
+        }
+        public bool HasNext()
+        {
+            return _stack.Any();
+        }
 
-        //public int Next()
-        //{
-            
-        //}
+        public int Next()
+        {
+            var node = _stack.Pop();
+            var tmp = node.Right;
+             
+            while (tmp != null)
+            {
+                _stack.Push(tmp);
+                tmp = tmp.Left;
+            }
+
+            return node.Val;
+        }
     }
 }
---
+++ TestLeetcode/BinaryTree/TestBinarySearchTreeIterator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Leetcode.BinaryTree;
+using Leetcode.DataStructure;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestLeetcode.BinaryTree
+{
+    [TestClass]
+    public class TestBinarySearchTreeIterator
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var node1 = new TreeNode(7);
+            var node2 = new TreeNode(3);
+            var node3 = new TreeNode(15);
+            var node4 = new TreeNode(9);
+            var node5 = new TreeNode(20);
+
+            node1.Left = node2;
+            node1.Right = node3;
+            node3.Left = node4;
+            node3.Right = node5;
+
+            var iterator = new BinarySearchTreeIterator(node1);
+            Assert.AreEqual(iterator.Next(), 3); // return 3
+            Assert.AreEqual(iterator.Next(), 7); // return 7
+            Assert.AreEqual(iterator.HasNext(), true); // return true
+            Assert.AreEqual(iterator.Next(), 9); // return 9
+            Assert.AreEqual(iterator.HasNext(), true); // return true
+            Assert.AreEqual(iterator.Next(), 15); // return 15
+            Assert.AreEqual(iterator.HasNext(),true); // return true
+            Assert.AreEqual(iterator.Next(), 20); // return 20
+            Assert.AreEqual(iterator.HasNext(), false); // return false
+        }
+    }
+}
