108789639ebe81806c1c4c1bab0792f67279d45c
warcraftpao <warcraftpao@gmail.com>
2019-01-31 11:25:44 +0800

    repeat dna sequences  没有完成

---
+++ Leetcode/Middle/RepeatedDNASequences.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Middle
+{
+    //不理解的是 这个重复的意思，比如 AAAGGGAAGGAAAGGGAAGG肯定是符合的
+    //但是比如 AG重复6次AGAGAGAGAGAG 这个repeat了吗，看网上的代码逻辑，这样也算的
+    public class RepeatedDNASequences
+    {
+    }
+
+    //https://blog.csdn.net/To_be_to_thought/article/details/85204687
+}
