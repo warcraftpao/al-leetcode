56f46f8adb46b4efeba6993f4908068645d07076
warcraftpao <warcraftpao@gmail.com>
2019-02-02 17:44:06 +0800

    binary tree right view way 1

---
+++ Leetcode/BinaryTree/BinaryTreeRightSideView.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Leetcode.DataStructure;
+
+namespace Leetcode.BinaryTree
+{
+    public   class BinaryTreeRightSideView
+    {
+        //层级遍历，每层结束的时候获得最右边的那个
+        public static List<int> LevelOrderWay(TreeNode root)
+        {
+            var list = new  List<int>();
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                //当前queue里有几个元素，说明本层有几个node
+                var size = queue.Count;
+                for (var i = 1; i <= size; i++)
+                {
+                    var node = queue.Dequeue();
+                    if (node != null)
+                    {
+                        if (node.Left != null) queue.Enqueue(node.Left);
+                        if (node.Right != null) queue.Enqueue(node.Right);
+                    }
+                    if (i == size)
+                    {
+                        list.Add(node.Val);
+                    }
+                }
+                 
+            }
+
+            return list;
+        }
+    }
+}
---
+++ TestLeetcode/BinaryTree/TestBinaryTreeRightView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Leetcode.BinaryTree;
+using Leetcode.DataStructure;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestLeetcode.BinaryTree
+{
+    [TestClass]
+    public class TestBinaryTreeRightView
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var node1 = new TreeNode(1);
+            var node2 = new TreeNode(2);
+            var node3 = new TreeNode(3);
+            var node4 = new TreeNode(4);
+            var node5 = new TreeNode(5);
+            var node6 = new TreeNode(6);
+
+
+            node1.Left = node2;
+            node1.Right = node3;
+
+            node2.Left = node5;
+            node3.Left = node4;
+
+            node5.Right = node6;
+
+            var r = BinaryTreeRightSideView.LevelOrderWay(node1);
+            Assert.AreEqual(string.Join("", r), "1346");
+        }
+    }
+}
