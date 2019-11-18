d6238f06bb087b3d1c3e781276a1aadc0de987bc
warcraftpao <warcraftpao@gmail.com>
2019-11-18 15:54:27 +0800

    contains duplicated 3

--- Leetcode/Easy/ContainsDulicate.cs
+++ Leetcode/Easy/ContainsDulicate.cs
@@ -50,5 +50,73 @@ namespace Leetcode.Easy
 
             return false;
         }
+
+
+        /// <summary>
+        /// //k坐标差值，t是2个元素的差值绝对值上限
+        ///在数组里，是否存在2个元素坐标差小于k且差值绝对值小于t
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="k">坐标差</param>
+        /// <param name="t">值差</param>
+        /// <returns></returns>
+        public static bool Level3(int[] nums, int k, int t)
+        {
+            //思路网上看的滑动窗口
+            //2个指针 x保持不动，y前进，保持xy差值小于k，并且y前进的时候把元素加入排序集合（sortedSet)，并且判断当前集合是否有满足要求的结果
+            //没有y继续前进，xy差值大于k的时候，x前进，并且队集合删掉坐标x的元素。
+
+            //现在问题变为 集合里面是否有有一个值满足和当前元素差的绝对值小于t
+            //找到当前集合里比x小的最大值，他们的差绝对值满足条件返回true(这个时候更小的值不用测试了）
+            //找到当前集合里比x大的最小值，他们的差绝对值满足条件返回true(这个时候更大的值不用测试了）
+
+            if (t < 0) return false; // 绝对值不会小于0
+            if (k < 0) return false; //坐标差至少是1
+            if (k > nums.Length -1) return false;//长度不可能超过数组长度-1 ，长度为4，差值最多为3
+
+            //排序集合
+            var set = new SortedSet<int>();
+            //遍历数组
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var curr = nums[i];
+                //包含这个元素差绝对值是0，肯定符合要求
+                if (set.Contains(curr)) return true;
+
+                //找到比当前元素小的最大值
+                if (set.Any(s => s < curr))
+                {
+                    var small = set.Where(s => s < curr).Max();
+                    if (Math.Abs(small - curr) < t)
+                        return true;
+                }
+
+                //找到比当前元素大的最小值
+                if (set.Any(s => s > curr))
+                {
+                    var big = set.Where(s => s  > curr).Min();
+                    if (Math.Abs(big - curr) < t)
+                        return true;
+                }
+
+                //当期元素加入集合
+                set.Add(curr);
+
+                //坐标差不超过k，说明最多允许k+1个连续元素一起比较
+                //比如k=2 当坐标值是2的时候 （坐标值大于等于k），说明已经3个元素（第1，2，3个元素参）加过比较了，
+                //下次循环的时候第一个元素用不到了（第i-k个元素删除，更早的肯定已经删除了）
+                if (i >= k)
+                {
+                    set.Remove(nums[i - k]);
+                }
+
+
+            }
+
+            return false;
+
+        }
+
+
     }
 }
--- TestLeetcode/FamilyTest/TestContainsDulicate.cs
+++ TestLeetcode/FamilyTest/TestContainsDulicate.cs
@@ -41,5 +41,61 @@ namespace TestLeetcode.FamilyTest
             r = ContainsDulicate.Level2(new[] { 1, 2, 3, 1, 2, 3,4,5,6,7,4 }, 3);
             Assert.IsTrue(r);
         }
+
+        [TestMethod]
+        public void TestSortedSet()
+        {
+            var set = new SortedSet<int>();
+            var r1 = set.Add(1);
+            var r1Again = set.Add(1);
+            set.Add(2);
+            set.Add(3);
+            set.Add(4);
+            set.Add(6);
+            set.Add(7);
+            set.Add(8);
+
+            //重复加返回false
+            Assert.IsTrue(r1);
+            Assert.IsFalse(r1Again);
+
+            //where 后toList还是保持本来的顺序
+            var where = set.Where(a => a < 6).ToList();
+            Assert.AreEqual(where[0] ,1);
+
+            if (set.Any(s => s < -1))
+            {
+                //不会执行到
+                var small = set.Where(s => s < -1).Max();
+            }
+
+
+            if (set.Any(s => s < 6))
+            {
+                //where后取max min还能取到的
+                var small = set.Where(s => s < 6).Max();
+                Assert.AreEqual(small, 4);
+            }
+
+        }
+
+
+        [TestMethod]
+        public void TestLevel3()
+        {
+            var r = ContainsDulicate.Level3(new[] { 1, 2, 3, 1 }, 3,0);
+            Assert.IsTrue(r);
+
+            r = ContainsDulicate.Level3(new[] { 1, 0, 1, 1 }, 1, 2);
+            Assert.IsTrue(r);
+
+            r = ContainsDulicate.Level3(new[] { 1, 5, 9, 1, 5, 9 }, 2 ,3);
+            Assert.IsFalse(r);
+
+
+            r = ContainsDulicate.Level3(new[] { -101, 200, -300, -50, 21, 3, 4, 9, 6, 7, 4 }, 2,6);
+            Assert.IsTrue(r);
+        }
+
     }
 }
